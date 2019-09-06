eaea6a8d8e51e61ba3da2f1853502e97c35235cb
Roderick@Job 3:14 <roderick@karlemstrand.com>
2019-09-06 20:45:48 +0200

    Jonas kod

---
+++ uppgift1.js
@@ -0,0 +1,15 @@
+var div = document.createElement("div");
+var paragraph = document.createElement("p");
+var text = document.createTextNode("Hej! Mitt namn är Jonas");
+p.appendChild("text");
+div.appendChild("p");
+
+
+var div2 = document.createElement("div");
+
+var bra = [
+    "bra1",
+    "bra2",
+    "bra3",
+    "bra4",
+];
